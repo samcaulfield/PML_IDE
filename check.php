bca0659e244a147c7631576fde7270ef4cb9d940
Sam Caulfield <caulfies@tcd.ie>
2016-02-07 12:16:15 +0000

    Change temp file prefix

--- check.php
+++ check.php
@@ -1,5 +1,5 @@
 <?php
-$temp = tempnam("/tmp", "cs4098");
+$temp = tempnam("/tmp", "PML_IDE");
 file_put_contents($temp, $_POST["value"]);
 echo shell_exec("/var/www/html/thirdparty/pmlcheck/pmlcheck $temp");
 ?>
