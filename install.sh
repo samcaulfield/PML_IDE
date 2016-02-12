8b4d53658a4f39b73676708f4e310431ae063034
Sam Caulfield <caulfies@tcd.ie>
2016-02-12 22:51:45 +0000

    Add argument checking to scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "No arguments supplied."
+	echo "Usage: # install.sh <projectName>"
+	exit 1
+fi
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root."
+	exit 1
+fi
+
 projectName=$1
 projectPath=/var/www/html/$projectName
 
