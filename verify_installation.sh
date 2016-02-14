edd390a904b8ceea3d873a7368a4b2dd80f82f1a
Sam Caulfield <caulfies@tcd.ie>
2016-02-14 23:14:09 +0000

    Improve usability of test script

--- verify_installation.sh
+++ verify_installation.sh
@@ -1,10 +1,25 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "No arguments supplied."
+	echo "Usage: $ verify_installation.sh <projectName>"
+	exit 1
+fi
+
 projectName=$1
 projectPath=/var/www/html/$projectName
 
 if [ ! -f $projectPath/index.html ] ||
+	[ ! -f $projectPath/login.html ] ||
+	[ ! -f $projectPath/register.html ] ||
 	[ ! -f $projectPath/check.php ] ||
+	[ ! -f $projectPath/register.php ] ||
+	[ ! -f $projectPath/login_style.css ] ||
 	[ ! -f $projectPath/StyleSheet.css ] ||
 	[ ! -d $projectPath/thirdparty/ ]; then
 	echo "There is a problem with the installation. Run clean.sh and then try install.sh again.";
+	exit
 fi
+
+echo "Everything seems to be OK.";
 
