9941477aa5b62c2d66794f11d7ccbdf1160beb20
Sam Caulfield <caulfies@tcd.ie>
2016-02-20 18:35:44 +0000

    Add file upload

--- install.sh
+++ install.sh
@@ -24,12 +24,15 @@ cp modalStyle.css $projectPath
 cp check.php $projectPath
 cp login.php $projectPath
 cp register.php $projectPath
+cp uploadFile.php $projectPath
 # Other.
 cp -R thirdparty/ $projectPath
 
 # Create the database.
 touch $projectPath/user.db
+mkdir $projectPath/userdata
 # Make it writeable to the server's PHP scripts (all users).
 chmod a+w $projectPath/user.db
+chmod a+w $projectPath/userdata
 chmod +777 $projectPath
 
---
+++ uploadFile.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$userEmailAddress = $_POST['email'];
+$uploadFileName = $_POST['fileName'];
+
+/* Create entry in db. */
+$db = new SQLite3('file.db');
+$db->query('create table if not exists file(email varchar(255), fileName varchar(255), primary key(email, fileName))');
+$queryResult = $db->querySingle("insert into file(email, fileName) values('$userEmailAddress', '$uploadFileName');");
+$db->close();
+
+/* Save string as file to disk. */
+if(is_dir("userdata/$userEmailAddress") === false) {
+    mkdir("userdata/$userEmailAddress");
+}
+file_put_contents("userdata/$userEmailAddress/$uploadFileName", $_POST['fileContents']);
+?>
+
