74cf804826b9bf75e76e6e9a92898ee7f8a78efb
Sharon Luk <luks@tcd.ie>
2016-04-06 13:04:11 +0100

    test for specifying file name to be saved

---
+++ Test/fileNameSpecification.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class T4(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_t4(self):
+        driver = self.driver
+        driver.get(self.base_url + "/RR13/")
+        driver.find_element_by_css_selector("button.navbar-toggle.collapsed").click()
+        driver.find_element_by_link_text("File").click()
+        driver.find_element_by_link_text("Save to disk").click()
+        driver.find_element_by_id("fileSaveToDiskNameInput").clear()
+        driver.find_element_by_id("fileSaveToDiskNameInput").send_keys("pml")
+        driver.find_element_by_css_selector("#fileSaveToDiskForm > button.btn.btn-default").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
