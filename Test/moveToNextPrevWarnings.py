b3cf37aa1d2f6a4dc4cffbdef4f1ba1ce7e6ad0f
Gulnur1 <ualiyevg@tcd.ie>
2016-04-09 00:19:44 +0100

    jump to next/previous warnings

--- Test/moveToNextPrevWarnings.py
+++ Test/moveToNextPrevWarnings.py
@@ -7,7 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class JumptoFirstNextWarnings(unittest.TestCase):
+class MoveToNextPrev(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -15,13 +15,13 @@ class JumptoFirstNextWarnings(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_jumpto_first_next_warnings(self):
+    def test_move_to_next_prev(self):
         driver = self.driver
         f = open('property')
         project_name = f.read()
         f.close()
         driver.get(self.base_url + "/" + project_name + "/")
-        print "\nNow testing for Jumping to first/next warnings"
+        print "\nNow testing for Jumping to next/previous warnings"
         driver.find_element_by_css_selector("div.ace_content").click()
         driver.find_element_by_class_name("ace_text-input").send_keys("""
 process simple {    
@@ -38,17 +38,17 @@ provides { bar }
 
         driver.find_element_by_link_text("Tools").click()
         driver.find_element_by_link_text("Check syntax").click()
-        # ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
+        driver.execute_script("moveToNext();")
+        print("Moved to next warning 2")
+        time.sleep(1)
 	driver.execute_script("moveToNext();")
-	print("Moved to warning 2")
-        #driver.find_element_by_xpath("(//a[contains(text(),'next')])[16]").click()
-        # ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
-	time.sleep(1)
-	print("Moved to warning 3")
-	driver.execute_script("moveToNext();")
-        #driver.find_element_by_xpath("(//a[contains(text(),'next')])[4]").click()
-        # ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
-    
+        print("Moved to next warning 3")
+        time.sleep(1)
+        driver.execute_script("moveToPrevious();")
+        print ("Moved to previous warning 2")
+        driver.execute_script("moveToPrevious();")
+        time.sleep(1)
+        print ("Moved to previous warning 1")
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
